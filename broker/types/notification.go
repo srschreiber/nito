07b096f5fa0c9b7fa39cf3ee001308fa3ec22ec0
Schreiber <sam.schreiber@sublimesecurity.com>
2026-03-19 09:10:28 -0700

    room select ui

---
+++ broker/types/notification.go
@@ -0,0 +1,5 @@
+package types
+
+type NotificationPayload struct {
+	Text string `json:"text"`
+}
---
+++ shellapp/components/room_members.go
@@ -0,0 +1,104 @@
+package components
+
+import (
+	"time"
+
+	tea "charm.land/bubbletea/v2"
+	lipgloss "charm.land/lipgloss/v2"
+	"github.com/srschreiber/nito/shellapp/connection"
+	"github.com/srschreiber/nito/shellapp/styles"
+	"github.com/srschreiber/nito/shellapp/types"
+)
+
+type membersPollMsg struct{}
+
+type RoomMembersComponent struct {
+	members []types.RoomMember
+	roomID  *string
+	width   int
+	height  int
+}
+
+func NewRoomMembersComponent(width, height int) *RoomMembersComponent {
+	return &RoomMembersComponent{width: width, height: height}
+}
+
+func (m *RoomMembersComponent) SetSize(width, height int) {
+	m.width = width
+	m.height = height
+}
+
+func (m *RoomMembersComponent) SetFocused(_ bool) {}
+
+func (m *RoomMembersComponent) Init() tea.Cmd {
+	return m.schedulePoll()
+}
+
+func (m *RoomMembersComponent) schedulePoll() tea.Cmd {
+	return tea.Tick(15*time.Second, func(time.Time) tea.Msg {
+		return membersPollMsg{}
+	})
+}
+
+func (m *RoomMembersComponent) fetch() tea.Cmd {
+	roomID := m.roomID
+	if roomID == nil {
+		return nil
+	}
+	id := *roomID
+	return func() tea.Msg {
+		members, err := connection.ListRoomMembers(id)
+		if err != nil {
+			return nil
+		}
+		return types.RoomMembersUpdatedMsg{Members: members}
+	}
+}
+
+func (m *RoomMembersComponent) Update(msg tea.Msg) tea.Cmd {
+	switch msg := msg.(type) {
+	case membersPollMsg:
+		return tea.Batch(m.fetch(), m.schedulePoll())
+	case types.RoomSelectedMsg:
+		m.roomID = &msg.RoomID
+		m.members = nil
+		return m.fetch()
+	case types.RoomMembersFetchMsg:
+		m.roomID = &msg.RoomID
+		return m.fetch()
+	case types.RoomMembersUpdatedMsg:
+		m.members = msg.Members
+	}
+	return nil
+}
+
+func (m *RoomMembersComponent) Render() string {
+	if m.width <= 0 {
+		return ""
+	}
+
+	title := styles.SectionTitleStyle.Render("Members")
+	body := title + "\n"
+
+	if len(m.members) == 0 {
+		body += styles.Grey.Render("  no members")
+	} else {
+		onlineStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("82"))
+		offlineStyle := lipgloss.NewStyle().Foreground(lipgloss.Color("196"))
+		for _, member := range m.members {
+			dot := offlineStyle.Render("●")
+			if member.Online {
+				dot = onlineStyle.Render("●")
+			}
+			body += styles.ItemStyle.Render(dot+" "+member.Username) + "\n"
+		}
+	}
+
+	return lipgloss.NewStyle().
+		Border(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("238")).
+		Padding(0, 1).
+		Width(m.width).
+		Height(m.height).
+		Render(body)
+}
