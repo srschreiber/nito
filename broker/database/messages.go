30a2d1c6b74c616d91d3c1a847b5c89c1d35becf
Schreiber <sam.schreiber@sublimesecurity.com>
2026-03-22 09:26:26 -0700

    refactor handlers

--- broker/database/messages.go
+++ broker/database/messages.go
@@ -5,5 +5,16 @@ import wstypes "github.com/srschreiber/nito/websocket_types"
 
 // InsertRoomMessage inserts a message into the database, returning an error if the room doesn't exist or the sender isn't a joined member.
 func InsertRoomMessage(ctx context.Context, conn Conn, payload wstypes.RoomMessagePayload) error {
+	const sql = `
+	INSERT INTO room_message (room_id, key_version_num, sender_message_count, sender_user_id, encrypted_text)
+	VALUES ($1, $2, $3, $4, $5)
+	`
+	_, err := conn.Exec(ctx, sql,
+		payload.RoomID, payload.RoomKeyVersion, payload.SenderMessageCount, payload.FromUserID, payload.EncryptedText,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
